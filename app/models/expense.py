ed4749a733298007cf5d2d13b5418c0d9e757644
Luiz Pericolo <luizpericolo@gmail.com>
2017-08-05 17:42:15 -0300

    add fields to expense model

--- app/models/expense.py
+++ app/models/expense.py
@@ -1,5 +1,7 @@
 class Expense:
-    def __init__(self, date, name, amount):
+    def __init__(self, date, name, amount, _id=None, user_id=None):
+        self._id = _id
         self.date = date
         self.name = name
         self.amount = amount
+        self.user_id = user_id
