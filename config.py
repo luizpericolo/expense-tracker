866501e160d01554a48c1301b501800f2d32b052
Luiz Pericolo <luizpericolo@gmail.com>
2017-08-05 02:11:09 -0300

    add config
    no passwords in github!

--- config.py
+++ config.py
@@ -1,12 +1,23 @@
+import os
+
 from pymongo import MongoClient
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'Put your secret key here'
-DB_NAME = 'blog'
+DB_NAME = 'expense_tracker'
 
-DATABASE = MongoClient()[DB_NAME]
-POSTS_COLLECTION = DATABASE.posts
+MONGO_DB_USER = os.environ.get("MONGO_DB_USER")
+MONGO_DB_PASSWORD = os.environ.get("MONGO_DB_PASSWORD")
+
+MONGODB_URI = "mongodb://{}:{}@ds135963.mlab.com:35963/{}".format(
+    MONGO_DB_USER, MONGO_DB_PASSWORD, DB_NAME)
+
+DATABASE = MongoClient(MONGODB_URI)[DB_NAME]
+TAGS_COLLECTION = DATABASE.tags
 USERS_COLLECTION = DATABASE.users
-SETTINGS_COLLECTION = DATABASE.settings
+EXPENSES_COLLECTION = DATABASE.expenses
+
+
+PASSWORD_HASH_METHOD = 'pbkdf2:sha512:10000'
 
 DEBUG = True
