b662e97d405851d30847ce4b75d2d45530fb73ee
Luiz Pericolo <luizpericolo@gmail.com>
2017-08-05 19:34:54 -0300

    import new repository methods

--- app/repository/__init__.py
+++ app/repository/__init__.py
@@ -1,2 +1,2 @@
-from .expense import find_expense, insert_expense, find_user_expenses
+from .expense import find_expense, insert_expense, find_user_expenses, update_expense, delete_expense
 from .user import find_user_by_name, insert_user, find_user
