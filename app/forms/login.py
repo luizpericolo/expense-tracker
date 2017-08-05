97e9ed2926cb653a69c50e272eb7f3856c9eb43c
Luiz Pericolo <luizpericolo@gmail.com>
2017-08-05 01:56:50 -0300

    add validator in signup and make pep8 complient

--- app/forms/login.py
+++ app/forms/login.py
@@ -2,6 +2,7 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(Form):
 
     username = StringField('Username', validators=[DataRequired()])
--- app/forms/signup.py
+++ app/forms/signup.py
@@ -1,9 +1,13 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
+
 
 class SignupForm(Form):
 
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        EqualTo('confirm_password', message='Passwords must match')
+    ])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
