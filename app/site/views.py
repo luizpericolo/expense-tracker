f17477ac5ec53d9a81bb6641a3f1e7afa82ea4f0
Luiz Pericolo <luizpericolo@gmail.com>
2017-08-05 03:52:54 -0300

    implement home endpoint

--- app/site/views.py
+++ app/site/views.py
@@ -1,8 +1,12 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, session, redirect, url_for
 
 site = Blueprint('expense', __name__, template_folder='../static/templates')
+
 
 @site.route('/')
 @site.route('/home')
 def home():
-    return render_template('home.html')
+    if 'username' in session:
+        return render_template('home.html')
+    else:
+        return redirect(url_for('auth.login_get'))
