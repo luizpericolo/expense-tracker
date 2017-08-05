19b82486e7ee32661ba6773391f74c967fae0afc
Luiz Pericolo <luizpericolo@gmail.com>
2017-08-05 17:42:31 -0300

    show latest expenses in user home

--- app/site/views.py
+++ app/site/views.py
@@ -1,12 +1,15 @@
 from flask import render_template, Blueprint, session, redirect, url_for
 
+from app.repository import find_user_expenses
+
 site = Blueprint('site', __name__, template_folder='../static/templates')
 
 
 @site.route('/')
 @site.route('/home')
 def home():
-    if 'username' in session:
-        return render_template('home.html')
+    if 'user_id' in session:
+        user_expenses = find_user_expenses(session.get('user_id'), limit=10)
+        return render_template('home.html', expenses=list(user_expenses))
     else:
         return redirect(url_for('auth.login_get'))
